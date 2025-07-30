5d815a66c3bb0cfa81fa75a47aa76cf56e3b3d02
mohanrajk-30 <iammohanraj2004@gmail.com>
2025-07-30 18:46:50 +0530

    Create DigitPrint.java

---
+++ DigitPrint.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        
+        System.out.printf("%05d\n", num1);
+
+        
+        System.out.printf("%5d\n", num2);
+    }
+}
