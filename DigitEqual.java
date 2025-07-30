ed8b3385f013054eb9996542491abdfa718034fe
mohanrajk-30 <iammohanraj2004@gmail.com>
2025-07-30 19:17:15 +0530

    Create DigitEqual.java

---
+++ DigitEqual.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[]){
+     Scanner scanner = new Scanner(System.in);   
+        int a,b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+    
+    if(a==b)
+        System.out.println("EQUAL");
+        
+    else
+        System.out.println("NOT EQUAL");
+    }
+    
+    
+}
