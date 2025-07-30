21412b784726d45131e29c1fdb739733c4780e0b
mohanrajk-30 <iammohanraj2004@gmail.com>
2025-07-30 19:18:11 +0530

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        a = scanner.nextInt();
+        if(a%2==0)
+            
+        {
+            System.out.println("EVEN");
+            
+        }
+    else {
+            System.out.println("ODD");
+        }
+}
+}
