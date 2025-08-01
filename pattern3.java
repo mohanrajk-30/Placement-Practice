68d98a3888f55066cbdaf6b4d6c96407b9084d45
mohanrajk-30 <iammohanraj2004@gmail.com>
2025-08-01 12:02:09 +0530

    Create pattern3.java

---
+++ pattern3.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int n,r,c;
+        n = scanner.nextInt();
+        for(r=1;r<=n;r++)
+        {
+            for(c=1;c<=n;c++){
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+            
+    }
+}
