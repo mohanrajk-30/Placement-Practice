7d2deac663f9d1a94b99d6fda597423f9c344a05
mohanrajk-30 <iammohanraj2004@gmail.com>
2025-08-06 15:44:41 +0530

    Create EvenOddPrintIInArray.java

---
+++ EvenOddPrintIInArray.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            if(arr[i]%2==0)
+                count++;
+        }
+        System.out.println("Odd = "+(n-count));
+        System.out.println("Even = "+count);
+    }
+}
